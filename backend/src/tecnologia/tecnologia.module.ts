d2193c63b3c1c6509e3351ac0a72fb1ed7380994
murita <douglasb008@gmail.com>
2025-03-19 16:57:05 -0300

    feat(Backend): add endpoint tecnologias

--- backend/src/tecnologia/tecnologia.module.ts
+++ backend/src/tecnologia/tecnologia.module.ts
@@ -1,4 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Module } from "@nestjs/common"
+import { TecnologiaController } from "./tecnologia.controller"
+import { TecnologiaProvider } from "./tecnologia.provider"
+import { DbModule } from "src/db/db.module"
+import { PrismaProvider } from "src/db/prisma.provider"
+import { Tecnologia } from "@core"
 
-@Module({})
+@Module({
+	controllers: [TecnologiaController],
+	providers: [TecnologiaProvider],
+	imports: [DbModule],
+})
 export class TecnologiaModule {}
