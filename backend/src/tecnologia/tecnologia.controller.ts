1a0212a0808518de0e57f8b83b421ed28155bea0
murita <douglasb008@gmail.com>
2025-03-20 09:49:40 -0300

    Fix(backend): rename tecnologia provider to prisma

--- backend/src/tecnologia/tecnologia.controller.ts
+++ backend/src/tecnologia/tecnologia.controller.ts
@@ -1,13 +1,18 @@
 import { Controller, Get } from "@nestjs/common"
 import { Tecnologia } from "@core"
-import { TecnologiaProvider } from "./tecnologia.provider"
+import { TecnologiaPrisma } from "./tecnologia.prisma"
 
 @Controller("tecnologias")
 export class TecnologiaController {
-	constructor(private readonly repo: TecnologiaProvider) {}
+	constructor(private readonly repo: TecnologiaPrisma) {}
 
 	@Get()
 	async obterTodas(): Promise<Tecnologia[]> {
 		return this.repo.obterTodas()
+	}
+
+	@Get("destaques")
+	async obterDestaques(): Promise<Tecnologia[]> {
+		return this.repo.obterDestaques()
 	}
 }
--- backend/src/tecnologia/tecnologia.prisma.ts
+++ backend/src/tecnologia/tecnologia.prisma.ts
@@ -3,10 +3,18 @@ import { Injectable } from "@nestjs/common"
 import { PrismaProvider } from "src/db/prisma.provider"
 
 @Injectable()
-export class TecnologiaProvider {
+export class TecnologiaPrisma {
 	constructor(private readonly prisma: PrismaProvider) {}
 
 	async obterTodas(): Promise<Tecnologia[]> {
 		return this.prisma.tecnologia.findMany()
+	}
+
+	async obterDestaques(): Promise<Tecnologia[]> {
+		return this.prisma.tecnologia.findMany({
+			where: {
+				destaque: true,
+			},
+		})
 	}
 }
